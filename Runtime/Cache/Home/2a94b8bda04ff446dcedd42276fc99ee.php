191daaaf25c3484844576ca0820e6a962aaac185
baijunyao <baijunyao@baijunyao.com>
2015-03-17 00:04:01 +0800

    布局前台首页

---
+++ Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,29 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>白俊遥的个人博客</title>
+<script type="text/javascript" src="/Public/static/js/jquery-2.0.0.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/Public/static/bootstrap-3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/Public/static/bootstrap-3.3.2/css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="/Public/static/font-awesome-4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="/Public/static/css/bjy.css">
+<script type="text/javascript" src="/Public/static/bootstrap-3.3.2/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/Public/static/iCheck-1.0.2/icheck.min.js"></script>
+<link rel="stylesheet" href="/Public/static/iCheck-1.0.2/skins/all.css">
+<script>
+$(document).ready(function(){
+  $('.icheck').iCheck({
+    checkboxClass: "icheckbox_square-blue",
+    radioClass: "iradio_square-blue",
+    increaseArea: "20%"
+  });
+});
+</script>
+</head>
+<body>
+<div id="">
+	
+</div>
+</body>
+</html>
