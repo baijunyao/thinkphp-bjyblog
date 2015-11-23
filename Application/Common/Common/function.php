08459f1e9aad9c6acd5146d4cf5fc2346f925118
baijunyao <baijunyao@baijunyao.com>
2015-11-23 23:22:48 +0800

    增加curl函数

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -316,3 +316,23 @@ function RU($url){
 	}
 	return $url;
 }
+
+/**
+ * 使用curl获取远程数据
+ * @param  string $url url连接
+ * @return string      获取到的数据
+ */
+function curl_get_contents($url){   
+    $ch = curl_init();   
+    curl_setopt($ch, CURLOPT_URL, $url);            	//设置访问的url地址   
+    //curl_setopt($ch,CURLOPT_HEADER,1);            	//是否显示头部信息   
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);           	//设置超时   
+    curl_setopt($ch, CURLOPT_USERAGENT, _USERAGENT_);   //用户访问代理 User-Agent   
+    curl_setopt($ch, CURLOPT_REFERER,_REFERER_);        //设置 referer   
+    curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);      	//跟踪301   
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);        //返回结果   
+    $r = curl_exec($ch);   
+    curl_close($ch);   
+    return $r;   
+}  
+  
