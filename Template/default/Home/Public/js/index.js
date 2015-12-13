c3201c1050fa65ed8ff33494bd6e5841b7fd84d7
baijunyao <baijunyao@baijunyao.com>
2015-12-13 16:01:27 +0800

    修复index.js中setCookie函数的错误

--- Template/default/Home/Public/js/index.js
+++ Template/default/Home/Public/js/index.js
@@ -90,10 +90,20 @@ function goTop(){
 	return false;
 }
 
-// 设置cookie
-function setCookie(name,value) {
-	var Days = 30;
-	var exp = new Date();
-	exp.setTime(exp.getTime() + Days*24*60*60*1000);
-	document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+/**
+ * 设置cookie
+ * @param {string} name  键名
+ * @param {string} value 键值
+ * @param {integer} days cookie周期
+ */
+function setCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }else{
+    	var expires = "";
+    }
+    document.cookie = name+"="+value+expires+"; path=/";
 }
+
