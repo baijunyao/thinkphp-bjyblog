4260f7c7bbfb0449e43849c276a1b3796b73312b
baijunyao <baijunyao@baijunyao.com>
2016-02-05 22:00:56 +0800

    使用百度cdn的jquery库；并创建回退地址

--- Application/Common/Tag/My.class.php
+++ Application/Common/Tag/My.class.php
@@ -4,97 +4,103 @@ namespace Common\Tag;
 use Think\Template\TagLib;
 
 class My extends TagLib {
-	// 定义标签
-	protected $tags=array(
-		'jquery'=>array('attr'=>'','close'=>0),
-		'animate'=>array('attr'=>'','close'=>0),
-		'bootstrapcss'=>array('','close'=>0),
-		'bootstrapjs'=>array('','close'=>0),
-		'icheckcss'=>array('','close'=>0),
-		'icheckjs'=>array('attr'=>'icheck','close'=>0),
-		'ueditor'=> array('attr'=>'name,content','close'=>0),
-		'recommend'=>array('attr'=>'limit','level'=>1)
-		);
+    // 定义标签
+    protected $tags=array(
+        'jquery'=>array('attr'=>'','close'=>0),
+        'animate'=>array('attr'=>'','close'=>0),
+        'bootstrapcss'=>array('','close'=>0),
+        'bootstrapjs'=>array('','close'=>0),
+        'icheckcss'=>array('','close'=>0),
+        'icheckjs'=>array('attr'=>'icheck','close'=>0),
+        'ueditor'=> array('attr'=>'name,content','close'=>0),
+        'recommend'=>array('attr'=>'limit','level'=>1)
+        );
 
-	//引入jquery
-	public function _jquery(){
-		return '<script type="text/javascript" src="__PUBLIC__/static/js/jquery-2.0.0.min.js"></script>';
-	}
+    //引入jquery
+    public function _jquery(){
+        return '<script type="text/javascript" src="__PUBLIC__/static/js/jquery-2.0.0.min.js"></script>';
+    }
 
-	//引入animate
-	public function _animate(){
-		return '<link rel="stylesheet" type="text/css" href="__PUBLIC__/static/css/animate.css">';
-	}
+    //引入animate
+    public function _animate(){
+        return '<link rel="stylesheet" type="text/css" href="__PUBLIC__/static/css/animate.css">';
+    }
 
-	/**
-	* bootstrap的css部分
-	*/
-	public function _bootstrapcss($tag){
-		$icheck=isset($tag['icheck']) ? $tag['icheck'] : 'blue';
-		$link=<<<php
+    /**
+    * bootstrap的css部分
+    */
+    public function _bootstrapcss($tag){
+        $icheck=isset($tag['icheck']) ? $tag['icheck'] : 'blue';
+        $link=<<<php
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<link rel="stylesheet" type="text/css" href="__PUBLIC__/static/bootstrap-3.3.5/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="__PUBLIC__/static/bootstrap-3.3.5/css/bootstrap-theme.min.css">
-	<link rel="stylesheet" type="text/css" href="__PUBLIC__/static/font-awesome-4.4.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="__PUBLIC__/static/css/bjy.css">
+    <link rel="stylesheet" type="text/css" href="__PUBLIC__/static/bootstrap-3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="__PUBLIC__/static/bootstrap-3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="__PUBLIC__/static/font-awesome-4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="__PUBLIC__/static/css/bjy.css">
 php;
-		return $link;
-	}
+        return $link;
+    }
 
-	/**
-	* 引入jquery、bootstrap的js部分
-	*/
-	public function _bootstrapjs(){
-		$link=<<<php
-<script type="text/javascript" src="__PUBLIC__/static/js/jquery-2.0.0.min.js"></script>
+    /**
+    * 引入jquery、bootstrap的js部分
+    */
+    public function _bootstrapjs(){
+        $link=<<<php
+<script type="text/javascript" src="//apps.bdimg.com/libs/jquery/2.0.0/jquery.min.js"></script>
+<script>
+(function(){
+	var BasePath='__PUBLIC__';
+	window.jQuery || document.write('<script src="'+BasePath+'/static/js/jquery-2.0.0.min.js"><\/script>');
+})();
+</script>
 <script type="text/javascript" src="__PUBLIC__/static/bootstrap-3.3.5/js/bootstrap.min.js"></script>
 <!--[if lt IE 9]>
 <script type="text/javascript" src="__PUBLIC__/static/js/html5shiv.min.js"></script>
 <script type="text/javascript" src="__PUBLIC__/static/js/respond.min.js"></script>
 <![endif]-->
 php;
-		return $link;
-	}
+        return $link;
+    }
 
-	/**
-	* 引入ickeck的css部分
-	*/
-	public function _icheckcss(){
-		$link=<<<php
-	<link rel="stylesheet" href="__PUBLIC__/static/iCheck-1.0.2/skins/all.css">
+    /**
+    * 引入ickeck的css部分
+    */
+    public function _icheckcss(){
+        $link=<<<php
+    <link rel="stylesheet" href="__PUBLIC__/static/iCheck-1.0.2/skins/all.css">
 php;
-		return $link;
-	}
+        return $link;
+    }
 
-	/**
-	* 引入ickeck的js部分
-	* @param string $tag  颜色主题
-	*/
-	public function _icheckjs($tag){
-		$color=isset($tag['color']) ? $tag['color'] : 'blue';
-		$link=<<<php
+    /**
+    * 引入ickeck的js部分
+    * @param string $tag  颜色主题
+    */
+    public function _icheckjs($tag){
+        $color=isset($tag['color']) ? $tag['color'] : 'blue';
+        $link=<<<php
 <script type="text/javascript" src="__PUBLIC__/static/iCheck-1.0.2/icheck.min.js"></script>
 <script>
 $(document).ready(function(){
-	$('.icheck').iCheck({
-		checkboxClass: "icheckbox_square-$color",
-		radioClass: "iradio_square-$color",
-		increaseArea: "20%"
-	});
+    $('.icheck').iCheck({
+        checkboxClass: "icheckbox_square-$color",
+        radioClass: "iradio_square-$color",
+        increaseArea: "20%"
+    });
 });
 </script>
 php;
-		return $link;
-	}
+        return $link;
+    }
 
-	/**
-	* 引入ueidter编辑器
-	* @param string $tag  name:表单name content：编辑器初始化后 默认内容
-	*/
-	public function _ueditor($tag){
-		$name=isset($tag['name']) ? $tag['name'] : 'content';
-		$content=isset($tag['content']) ? $tag['content'] : '';
-		$link=<<<php
+    /**
+    * 引入ueidter编辑器
+    * @param string $tag  name:表单name content：编辑器初始化后 默认内容
+    */
+    public function _ueditor($tag){
+        $name=isset($tag['name']) ? $tag['name'] : 'content';
+        $content=isset($tag['content']) ? $tag['content'] : '';
+        $link=<<<php
 <script id="container" name="$name" type="text/plain">$content</script>
 <script type="text/javascript" src="__PUBLIC__/static/ueditor1_4_3/ueditor.config.js"></script>
 <script type="text/javascript" src="__PUBLIC__/static/ueditor1_4_3/ueditor.all.js"></script>
@@ -102,29 +108,29 @@ php;
     var ue = UE.getEditor('container');
 </script>
 php;
-		return $link;
-	}
+        return $link;
+    }
 
-	// 置顶推荐文章标签 cid为空时则抓取全部分类下的推荐文章
-	public function _recommend($tag,$content){
-	 	if(empty($tag['cid'])){
-	 		$where="is_show=1 and is_delete=0 and is_top=1";
-	 	}else{
-	 		$where='is_show=1 and is_delete=0 and is_top=1 and cid='.$tag['cid'];
-	 	}
-	 	$limit=$tag['limit'];
-	 	// p($recommend);
-	 	$php=<<<php
+    // 置顶推荐文章标签 cid为空时则抓取全部分类下的推荐文章
+    public function _recommend($tag,$content){
+        if(empty($tag['cid'])){
+            $where="is_show=1 and is_delete=0 and is_top=1";
+        }else{
+            $where='is_show=1 and is_delete=0 and is_top=1 and cid='.$tag['cid'];
+        }
+        $limit=$tag['limit'];
+        // p($recommend);
+        $php=<<<php
 <?php
-			\$recommend=M('Article')->field('aid,title')->where("$where")->limit($limit)->select();
-			foreach (\$recommend as \$k => \$field) {
-				\$url=U('Home/Index/article',array('aid'=>\$field['aid']));
+            \$recommend=M('Article')->field('aid,title')->where("$where")->limit($limit)->select();
+            foreach (\$recommend as \$k => \$field) {
+                \$url=U('Home/Index/article',array('aid'=>\$field['aid']));
 ?>
 php;
-		$php.=$content;
-		$php.='<?php } ?>';//foreach的回扩;
-		return $php;
-	 }
+        $php.=$content;
+        $php.='<?php } ?>';//foreach的回扩;
+        return $php;
+     }
 
 
 
