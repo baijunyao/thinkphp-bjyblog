961874d2c12630792390ed26c0a135640e65722d
baijunyao <baijunyao@baijunyao.com>
2016-06-17 22:57:19 +0800

    取消标签形式获取的置顶推荐

--- Application/Common/Controller/HomeBaseController.class.php
+++ Application/Common/Controller/HomeBaseController.class.php
@@ -14,11 +14,22 @@ class HomeBaseController extends BaseController{
             $this->display('Public/web_close');
             die;
         }
+        $recommend_map=array(
+            'is_show'=>1,
+            'is_delete'=>0,
+            'is_top'=>1
+            );
+        $recommend=M('Article')
+            ->field('aid,title')
+            ->where($recommend_map)
+            ->order('aid desc')
+            ->select();
         // 分配常用数据
         $assign=array(
             'categorys'=>D('Category')->getAllData(),
             'tags'=>D('Tag')->getAllData(),
             'links'=>D('Link')->getDataByState(0,1),
+            'recommend'=>$recommend
             );
         $this->assign($assign);
     }
