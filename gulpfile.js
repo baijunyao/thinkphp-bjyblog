78147bb6bca6dc95f54b64568b4fd89b81aa7d5d
baijunyao <baijunyao@baijunyao.com>
2016-11-18 23:28:33 +0800

    定义gulp源代码和编译压缩后的目录

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,42 @@ var gulp        = require('gulp'),
     imagemin    = require('gulp-imagemin');
     browserSync = require('browser-sync').create(),
     reload      = browserSync.reload;
+    
+// 定义源代码的目录和编译压缩后的目录
+var src='Template/default_src',
+    dist='Template/default'
 
 // 编译全部less 并压缩
 gulp.task('css', function(){
-    gulp.src('Template/default_src/**/*.less')
+    gulp.src(src+'/**/*.less')
         .pipe(less())
         .pipe(minifyCss())
-        .pipe(gulp.dest('Template/default'))
+        .pipe(gulp.dest(dist))
 })
 
 // 编译全部js 并压缩
 gulp.task('js', function() {
-  gulp.src('Template/default_src/**/*.js')
+  gulp.src(src+'/**/*.js')
     .pipe(plumber())
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('Template/default'));
+    .pipe(gulp.dest(dist));
 });
 
 // 压缩全部html
 gulp.task('html', function () {
-    gulp.src('Template/default_src/**/*.html')
+    gulp.src(src+'/**/*.html')
     .pipe(clearnHtml())
-    .pipe(gulp.dest('Template/default'));
+    .pipe(gulp.dest(dist));
 });
 
 // 压缩全部html
 gulp.task('image', function () {
-    gulp.src(['Template/default_src/**/image/*'])
+    gulp.src([src+'/**/image/*'])
     .pipe(imagemin())
-    .pipe(gulp.dest('Template/default'));
+    .pipe(gulp.dest(dist));
 });
 
 // 自动刷新
@@ -50,13 +54,13 @@ gulp.task('server', function() {
         open: false, // 不自动打开浏览器
     });
     // 监听less文件编译
-    gulp.watch('Template/default_src/**/*.less', ['css']);   
+    gulp.watch(src+'/**/*.less', ['css']);   
     // 监听html文件变化后刷新页面
-    gulp.watch("Template/default_src/**/*.js", ['js']).on("change", reload);
+    gulp.watch(src+"/**/*.js", ['js']).on("change", reload);
     // 监听html文件变化后刷新页面
-    gulp.watch("Template/default_src/**/*.html", ['html']).on("change", reload);
+    gulp.watch(src+"/**/*.html", ['html']).on("change", reload);
     // 监听css文件变化后刷新页面
-    gulp.watch("Template/default_src/**/*.css").on("change", reload);
+    gulp.watch(src+"/**/*.css").on("change", reload);
 });
 
 // 监听事件
