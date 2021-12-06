3254d6c3ca37c30c0b27c12648ee9c2bfb8a9d9d
baijunyao <baijunyao@baijunyao.com>
2021-12-06 23:53:32 +0800

    Fix XSS vulnerability (#6)

--- ThinkPHP/Common/functions.php
+++ ThinkPHP/Common/functions.php
@@ -1033,24 +1033,31 @@ function is_ssl() {
  * @param string $msg 重定向前的提示信息
  * @return void
  */
-function redirect($url, $time=0, $msg='') {
+function redirect($url, $time = 0, $msg = '')
+{
     //多行URL地址支持
-    $url        = str_replace(array("\n", "\r"), '', $url);
-    if (empty($msg))
-        $msg    = "系统将在{$time}秒之后自动跳转到{$url}！";
+    $url = str_replace(array("\n", "\r"), '', $url);
+    $url = htmlspecialchars(strip_tags($url), ENT_QUOTES, 'UTF-8', false);
+
+    if (empty($msg)) {
+        $msg = "系统将在{$time}秒之后自动跳转到{$url}！";
+    }
+
     if (!headers_sent()) {
         // redirect
         if (0 === $time) {
             header('Location: ' . $url);
         } else {
             header("refresh:{$time};url={$url}");
-            echo($msg);
+            echo ($msg);
         }
         exit();
     } else {
-        $str    = "<meta http-equiv='Refresh' content='{$time};URL={$url}'>";
-        if ($time != 0)
+        $str = "<meta http-equiv='Refresh' content='{$time};URL={$url}'>";
+        if (0 != $time) {
             $str .= $msg;
+        }
+
         exit($str);
     }
 }
